c25facb5aa385e804013f7379d00f9d8ed9a5b0a
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-08-20 13:01:32 -0600

    initial prototype finished

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let health = 100
 let enemyName = "Bob"
 let hits = 0
-
+let nameArray = ["John", "Mike", "Lewis", "Luke", "Nathan","Kholgan", "Grug","Hunter","Darius","Tezzert","Gideon", "Amber", "Sarah","Julie"];
+let knockouts = 0
 function slap() {
     health--
     hits++
     update()
+}
+
+function nameGenerator(){
+    let random = Math.floor(Math.random()*(nameArray.length-.001))
+    enemyName = nameArray[random]
 }
 
 function punch() {
@@ -20,11 +26,21 @@ function kick() {
     update()
 }
 
+function respawn(){
+    health = 100
+    nameGenerator()
+    knockouts++
+    update()
+}
+
 function update() {
     document.getElementById("health").innerText = health.toString()
     document.getElementById("enemyName").innerText = enemyName
     document.getElementById("hits").innerText = hits.toString()
-
+    document.getElementById("knockouts").innerText = knockouts.toString()
+    if(health <= 0){
+        respawn()
+    }
 }
-
+nameGenerator()
 update()
